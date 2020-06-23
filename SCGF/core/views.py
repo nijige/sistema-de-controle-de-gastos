95cd7062bbba06d72f7f7097908916e23835a381
Geane Ferreira <geanepereira@seduc.ro.gov.br>
2020-06-23 10:17:32 -0400

    adicionar a pagina login

--- SCGF/core/views.py
+++ SCGF/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def login(request):
+    return render(request,'pages/login.html')
 
 def home(request):
     return render(request, 'pages/home.html')
