72b33c63f103cbf0110ad361c34430d204aba611
Dieisson Narde <dieissonnarde@gmail.com>
2020-06-23 01:16:56 -0300

    Implementado visualização das transações do banco na page Home

--- SCGF/core/views.py
+++ SCGF/core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Transacao
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'pages/home.html')
+    transacoes = Transacao.objects.all()
+    return render(request, 'pages/home.html', {'transacoes': transacoes})
 
 def group(request):
     return render(request, 'pages/group.html')
