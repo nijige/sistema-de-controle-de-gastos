a8a6cda851ec7ea0941cf23760fa43d462654f14
Dieisson Narde <dieissonnarde@gmail.com>
2020-06-23 17:43:48 -0300

    Implementado a edicão, delete e message das transações

--- SCGF/core/urls.py
+++ SCGF/core/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('group/', views.group, name='group'),
-    path('transaction/', views.newTransaction),
+    path('transaction/', views.newTransaction, name='add-transaction'),
+    path('edit/<int:id>', views.editTransaction, name='edit-transaction'),
+    path('delete/<int:id>', views.deleteTransaction, name='delete-transaction'),
 ]
