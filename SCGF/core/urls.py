b868ed7038cc06d700d0c8742dc096b16ba794a5
kleydson7 <kleydsonclaudio7@gmail.com>
2020-06-26 15:52:51 -0300

    alteracoes

--- SCGF/core/urls.py
+++ SCGF/core/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('group/', views.group, name='group'),
+    path('transaction/', views.newTransaction, name='add-transaction'),
+    path('edit/<int:id>', views.editTransaction, name='edit-transaction'),
+    path('delete/<int:id>', views.deleteTransaction, name='delete-transaction'),
+    path('login/' ,views.login)
 ]
